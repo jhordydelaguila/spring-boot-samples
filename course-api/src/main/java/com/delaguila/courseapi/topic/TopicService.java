311f3fb258da36cc5b883cb70b2ba734ab7f2561
Jhordy Alexi <jhordydelaguila@hotmail.com>
2019-08-30 04:23:11 -0500

    Add addTopic()

--- course-api/src/main/java/com/delaguila/courseapi/topic/TopicService.java
+++ course-api/src/main/java/com/delaguila/courseapi/topic/TopicService.java
@@ -2,17 +2,20 @@ package com.delaguila.courseapi.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    //Logger logger = LoggerFactory.getLogger(TopicService.class);
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "spring framework", "spring framework description"),
             new Topic("java", "core java", "core java description"),
             new Topic("php", "Core PHP", "Core PHP description.")
-    );
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -20,6 +23,10 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 
 }
